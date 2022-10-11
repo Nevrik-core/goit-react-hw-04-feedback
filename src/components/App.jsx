8d78cdf2f505674ec0110f7f8a8fc65a590ebfe5
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-10-11 22:27:39 +0200

    Task 1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,57 @@
-import FeedbackWidget from "./FeedbackWidget/FeedbackWidget";
 
-export const App = () => {
-  return (
+import React from "react";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Notification } from "./Notification/Notification";
+import { Container, Header } from "./App.styled";
+
+export class App extends React.Component {
+
+  state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    onLeaveFeedback = (prop) => {
+        this.setState(state => ({
+            [prop]: state[prop] + 1
+        }));
+    }; 
+
+    countTotalFeedback =() => {
+        return Object.values(this.state).reduce((acc, el) => acc + el, 0);
+    }
+    
+    countPositiveFeedbackPercentage = () => {
+        return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+    }
+
+
+  render() {
+
+        const { good, neutral, bad } = this.state;
+
+    return (
     <div>
-      <FeedbackWidget /> 
+        <Container>
+        <Header>Please leave feedback</Header>
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.onLeaveFeedback} />
+        {this.countTotalFeedback()
+        ? <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()} />
+        : <Notification message="There is no feedback"></Notification> 
+        }
+        
+          </Container>
     </div>
   );
+  }
+  
 };
--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -1,22 +1,5 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-    background-color: white;
-    margin: 0 auto;
-    margin-top: 25vh;
-    margin-bottom: 30px;
-    width: 700px;
-    max-width: 85vw;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    box-shadow: ${props => props.theme.filters.shadow};
-`;
-
-export const Header = styled.h1`
-text-align: center;
-text-transform: uppercase;
-`;
-
 export const Wrapper = styled.div`
 display: flex;
 margin: 0 auto;
@@ -51,12 +34,4 @@ background-color: #F2BF5E;
     background-color: rgb(45, 45, 45);
     color: ${switchColor};
 }
-`;
-
-export const StatWrapper = styled.div`
-    /* margin: 0; */
-    text-align:center;
-    text-transform: uppercase;
-    line-height: 8px;
-    margin-top: 40px;
 `;
